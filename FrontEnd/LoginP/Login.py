1b4a53a1e9dc4990d920c00eb01e0ad721605e0f
ImedZeiri <61678189+ImedZeiri@users.noreply.github.com>
2022-06-26 16:48:21 +0100

    Login With Bg Confirmed

--- FrontEnd/LoginP/Login.py
+++ FrontEnd/LoginP/Login.py
@@ -1,56 +1,74 @@
 import time
 from tkinter import *
+from PIL import ImageTk, Image
 from tkinter import ttk
 import sqlite3
+
+from PIL import ImageTk
+
 from BackEnd.Services.Auth.Login import Login_Service
 from BackEnd.Routers.Role import UserRole
+from BackEnd.Services.Talk import Talk
 
 class Login:
     def mainLogin():
         Window = Tk()
         Window.title("Login")
-        Window.geometry("1041x588")
-        Window.maxsize(1041, 588)
-        Window.minsize(1041, 588)
-        Label1 = Label(Window)
-        Label1.place(relx=0.05, rely=0.11, height=21, width=100)
-        Label1.configure(background="white")
-        Label1.configure(disabledforeground="#a3a3a3")
-        Label1.configure(foreground="#000000")
-        Label1.configure(text='''Email :''', anchor=W)
+        Window.geometry("1141x628")
+        Window.minsize(1141, 628)
+        Window.maxsize(1141, 628)
+        bg = ImageTk.PhotoImage(file="../Assets/BackGround.png")
+        canvas = Canvas(Window, width=700, height=3500)
+        canvas.pack(fill=BOTH, expand=True)
+        canvas.create_image(0, 0, image=bg, anchor='nw')
+
+        def resize_image(e):
+            global image, resized, image2
+            image = Image.open("../Assets/BackGround.png")
+            resized = image.resize((e.width, e.height), Image.ANTIALIAS)
+            image2 = ImageTk.PhotoImage(resized)
+            canvas.create_image(0, 0, image=image2, anchor='nw')
+        Window.bind("<Configure>", resize_image)
 
         EntryEmail = Entry(Window)
-        EntryEmail.place(relx=0.22, rely=0.11, height=20, relwidth=0.191)
-
-        Label2 = Label(Window)
-        Label2.place(relx=0.05, rely=0.198, height=21, width=100)
-        Label2.configure(background="white")
-        Label2.configure(disabledforeground="#a3a3a3")
-        Label2.configure(foreground="#000000")
-        Label2.configure(text='''Password :''', anchor=W)
+        EntryEmail.place(relx=0.162, rely=0.464, height=30, relwidth=0.221)
+        EntryEmail.configure(background="white")
+        EntryEmail.configure(disabledforeground="white", bd=0)
+        EntryEmail.configure(font="TkFixedFont")
+        EntryEmail.configure(highlightbackground = "white", highlightcolor= "white")
+        EntryEmail.configure(insertbackground="black")
+        EntryEmail.insert(0, 'Email')
 
         EntryPassword = Entry(Window)
-        EntryPassword.place(relx=0.22, rely=0.286, height=20, relwidth=0.191)
+        EntryPassword.place(relx=0.162, rely=0.584, height=30, relwidth=0.221)
+        EntryPassword.configure(background="white")
+        EntryPassword.configure(disabledforeground="white", bd=0)
+        EntryPassword.configure(font="TkFixedFont")
+        EntryPassword.configure(highlightbackground = "white", highlightcolor= "white")
+        EntryPassword.configure(insertbackground="black", show="*")
+        EntryPassword.insert(0, 'Password')
 
         def destroy():
             time.sleep(1)
             Window.destroy()
 
-
         def Se_connecter():
-            Email = EntryEmail.get()
-            Password = EntryPassword.get()
-            resultat = [Email,Password]
-            a = Login_Service.Specification_Role(resultat)
-            destroy()
-            b = UserRole.Routage(a)
-            b
-
-
-
-
+            try:
+                Email = EntryEmail.get()
+                Password = EntryPassword.get()
+                resultat = [Email,Password]
+                Role = Login_Service.Specification_Role(resultat)
+                destroy()
+                TextToTalk = ("welcome  "+Role)
+                say = Talk.Talk(TextToTalk)
+                say
+                RunWindowUser = UserRole.Routage(Role)
+                RunWindowUser
+            except:
+                return False
+        LgImg = PhotoImage(file="../Assets/BTN/BouttonConnexion.png")
         bConnexin = Button(Window)
-        bConnexin.place(relx=0.593, rely=0.80, height=33, width=174)
+        bConnexin.place(relx=0.3, rely=0.690, height=40, width=144)
         bConnexin.configure(activebackground="#ececec")
         bConnexin.configure(activeforeground="#000000")
         bConnexin.configure(background="#d9d9d9")
@@ -59,7 +77,7 @@ class Login:
         bConnexin.configure(highlightbackground="#d9d9d9")
         bConnexin.configure(highlightcolor="black")
         bConnexin.configure(pady="0", bd=0)
-        bConnexin.configure(text="Se Connecter")
+        bConnexin.configure(image=LgImg)
         bConnexin.configure(command=Se_connecter)
 
         Window.mainloop()
